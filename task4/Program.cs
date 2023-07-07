f5d5da04b1278e716e2681938cc55d752f1d9ff6
okonoki <okonokiss@gmail.com>
2023-07-07 17:36:23 +0500

    modified task4

--- task4/Program.cs
+++ task4/Program.cs
@@ -9,26 +9,7 @@ Console.Write("Введите второе число: ");
 int number2 = int.Parse(Console.ReadLine());
 Console.Write("Введите третье число: ");
 int number3 = int.Parse(Console.ReadLine());
-
-if (number1 > number2)
-{
-    if (number1 > number3)
-    {
-        Console.Write(number1);
-    }
-    else
-    {
-        Console.Write(number3);
-    }
-}
-else
-{
-    if (number2 > number3)
-    {
-        Console.Write(number2);
-    }
-    else
-    {
-        Console.Write(number3);
-    }
-}
+int max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+Console.Write("Максимальное число: " + max);
