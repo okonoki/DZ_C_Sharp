f35690bcc11fee657b7768edfefa662ec6bf2a75
okonoki <okonokiss@gmail.com>
2023-07-08 20:26:30 +0500

    modified task8_2

--- task8/Program.cs
+++ task8/Program.cs
@@ -10,3 +10,11 @@ while (number <= N)
     Console.Write(number + ", ");
     number = number + 2;
 }
+
+Console.WriteLine("Введите число N:");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Чётные числа от 1 до " + N + ":");
+{
+for (int i = 2; i <= N; i += 2)
+Console.WriteLine(i);
+}
